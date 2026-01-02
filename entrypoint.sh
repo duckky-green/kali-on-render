4e4a11c466c13593cf3da5b7f78543f890b33c30
duckky-green <exovateofficial@gmail.com>
2026-01-02 11:32:08 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 export USER=root
 export HOME=/root
+export DISPLAY=:1
 
-# MANDATORY: Remove old VNC locks so it can start on :1
-rm -rf /tmp/.X* /tmp/.unix*
+# 1. Mandatory Cleanup for Render restarts
+rm -rf /tmp/.X11-unix /tmp/.X1-lock
 
-# Start VNC server (Internal display :1)
-vncserver :1 -geometry 1280x720 -depth 24 -SecurityTypes None
+# 2. Initialize VNC with a basic password (kali2026) 
+# Some versions of VNC crash if NO password is set even with -SecurityTypes None
+mkdir -p ~/.vnc
+echo "kali2026" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
 
-# Start noVNC proxy (Publicly accessible)
+# 3. Start VNC and force XFCE to launch
+vncserver :1 -geometry 1280x720 -depth 24 -rfbauth ~/.vnc/passwd
+
+# 4. Start noVNC proxy
 /usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 54321 --web /usr/share/novnc
