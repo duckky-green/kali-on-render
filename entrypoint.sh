dfbd9af399fca21309a2008735ec9a89ca4e2ead
duckky-green <exovateofficial@gmail.com>
2026-01-02 11:14:26 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Define the USER for VNC (mandatory in 2026)
 export USER=root
 export HOME=/root
 
-# 1. Start VNC server on port 5901 (Internal)
+# MANDATORY: Remove old VNC locks so it can start on :1
+rm -rf /tmp/.X* /tmp/.unix*
+
+# Start VNC server (Internal display :1)
 vncserver :1 -geometry 1280x720 -depth 24 -SecurityTypes None
 
-# 2. Start noVNC proxy on Render's required port
-# Corrected path for 2026: /usr/share/novnc/utils/novnc_proxy
-/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 54321
+# Start noVNC proxy (Publicly accessible)
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 54321 --web /usr/share/novnc
